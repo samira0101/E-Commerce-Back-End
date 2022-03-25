6a1fb4d075e00df6f2990f0dbfbe1a5e360bf1b4
Allure <samira-h@hotmail.co.uk>
2022-03-25 18:22:25 +0000

    get one product by ID

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -28,3 +28,29 @@ router.get('/', async (req, res) => {
 			res.json(err);
 		});
 });
+
+// get one product
+router.get('/:id', (req, res) => {
+  // find a single product by its `id`
+  // be sure to include its associated Category and Tag data
+  Product.findByPk(req.params.id, {
+		include: [
+			{
+				model: Tag,
+				attributes: ["id", "tag_name"],
+				through: "ProductTag",
+			},
+			{
+				model: Category,
+				attributes: ["id", "category_name"],
+			},
+		],
+	})
+		.then((specificProduct) => {
+			res.json(specificProduct);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
+
