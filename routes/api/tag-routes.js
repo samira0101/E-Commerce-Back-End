f3a37fc4d69e0d63fd225b5fe1f9dc1666b8ea86
Allure <samira-h@hotmail.co.uk>
2022-03-25 18:34:21 +0000

    get all tags

---
+++ routes/api/tag-routes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', async (req, res) => {
+  // find all tags
+  // be sure to include its associated Product data
+  await Tag.findAll({
+    attributes: ["id", "tag_name"],
+    include: [{
+      model: Product,
+      attributes: ["id", "product_name", "price", "stock", "category_id"],
+      through: "ProductTag",
+    }, ],
+  })
+  .then((parsedTagData) => {
+    res.json(parsedTagData);
+  })
+  .catch((err) => {
+    res.json(err);
+  });
+});
+
