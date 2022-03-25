9d15e6a8046ab048a44354c57809c4d78729bc62
Allure <samira-h@hotmail.co.uk>
2022-03-25 18:12:10 +0000

    edits to prodect tag

--- models/index.js
+++ models/index.js
@@ -11,3 +11,25 @@ Product.belongsTo(Category);
 Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    foreignKey: 'product_id'
+  }
+})
+
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through:{
+    model: ProductTag,
+    foreignKey: 'tag_id'
+  }
+})
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
