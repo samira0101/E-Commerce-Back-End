1cd8baa307b3e4e23f056df5f24f74a17c3f6af1
Allure <samira-h@hotmail.co.uk>
2022-03-25 18:11:26 +0000

    edits to product and category

---
+++ models/index.js
@@ -0,0 +1,13 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category);
+
+// Categories have many Products
+Category.hasMany(Product);
+
+// Products belongToMany Tags (through ProductTag)
